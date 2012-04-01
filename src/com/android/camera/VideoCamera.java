525ec9bc780ddbaabcd5976184d696d85a929b1f
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-04-01 22:11:34 +0200

    camera: allow setting parameter "video-size" before recording starts
    Change-Id: I5417304344dcc1cded76665111c85346d7712a15

--- src/com/android/camera/VideoCamera.java
+++ src/com/android/camera/VideoCamera.java
@@ -1793,6 +1793,9 @@ public class VideoCamera extends ActivityBase
         // Set video mode
         CameraSettings.setVideoMode(mParameters, true);
 
+        // Set video size before recording starts
+        CameraSettings.setEarlyVideoSize(mParameters, mProfile);
+
         mParameters.setPreviewSize(mDesiredPreviewWidth, mDesiredPreviewHeight);
         mParameters.setPreviewFrameRate(mProfile.videoFrameRate);
 
