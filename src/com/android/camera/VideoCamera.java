8c436295037541dc1a97c7aec3d890951d3c0ee5
Steve Kondik <shade@chemlab.org>
2012-05-16 18:29:11 -0700

    camera: Add debug logging when setting parameters
    Change-Id: Ida35e08e02f51b7ca2b89dfeeb7e550e6e562c25

--- src/com/android/camera/VideoCamera.java
+++ src/com/android/camera/VideoCamera.java
@@ -858,15 +858,8 @@ public class VideoCamera extends ActivityBase
             }
         }
 
-<<<<<<< HEAD
         setDisplayOrientation();
         mCameraDevice.setDisplayOrientation(mCameraDisplayOrientation);
-=======
-        mDisplayRotation = Util.getDisplayRotation(this);
-        int orientation = Util.getDisplayOrientation(mDisplayRotation, mCameraId);
-        mCameraDevice.setDisplayOrientation(orientation);
-        CameraSettings.setVideoMode(mParameters, true);
->>>>>>> e62c08c... Fix galaxys2 video recording
         setCameraParameters();
 
         try {
@@ -1872,6 +1865,7 @@ public class VideoCamera extends ActivityBase
                 CameraProfile.QUALITY_HIGH);
         mParameters.setJpegQuality(jpegQuality);
 
+        CameraSettings.dumpParameters(mParameters);
         mCameraDevice.setParameters(mParameters);
         // Keep preview size up to date.
         mParameters = mCameraDevice.getParameters();
@@ -2387,6 +2381,7 @@ public class VideoCamera extends ActivityBase
         mParameters.setRotation(rotation);
         Location loc = mLocationManager.getCurrentLocation();
         Util.setGpsParameters(mParameters, loc);
+        CameraSettings.dumpParameters(mParameters);
         mCameraDevice.setParameters(mParameters);
 
         Log.v(TAG, "Video snapshot start");
